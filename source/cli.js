7a54ad487391815ea52853baee7c4c0681cde0a9
Arnelle Balane <arnellebalane@gmail.com>
2019-01-23 18:18:22 +0800

    Colorize help output and display spinner

--- source/cli.js
+++ source/cli.js
@@ -2,27 +2,31 @@
 import '@babel/polyfill';
 import ora from 'ora';
 import meow from 'meow';
+import chalk from 'chalk';
 import devComic from '.';
 
 const cli = meow(`
-    Usage:
-      $ devcomic [source ...]
+    ${chalk.dim.underline('Usage:')}
+      ${chalk.dim('$')} ${chalk.green('devcomic')} ${chalk.dim('[')}${chalk.yellow('source')} ${chalk.dim('...]')}
 
-    Sources:
-      commitstrip
-      hacktoon
+    ${chalk.dim.underline('Sources:')}
+      ${chalk.yellow('commitstrip')}
+      ${chalk.yellow('hacktoon')}
 
-    Examples:
-      $ devcomic
-      $ devcomic commitstrip
-      $ devcomic commitstrip hacktoon
+    ${chalk.dim.underline('Examples:')}
+      ${chalk.dim('$')} ${chalk.green('devcomic')}
+      ${chalk.dim('$')} ${chalk.green('devcomic')} ${chalk.yellow('commitstrip')}
+      ${chalk.dim('$')} ${chalk.green('devcomic')} ${chalk.yellow('commitstrip hacktoon')}
 `);
 
 const sources = cli.input;
 
-const spinner = ora('Fetching comics');
+const spinner = ora(chalk.cyan('Fetching comics'));
 
 (async () => {
+    spinner.start();
     const response = await devComic({sources});
+    spinner.stop();
+
     console.log(response);
 })();
